9593693ab92a45af7aebd9fcfe38ab8d08c87a51
Yamil <yamilgiron98@gmail.com>
2025-05-17 18:20:12 -0400

    commit final

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
+// Importa addIcons y el ícono deseado
+import { addIcons } from 'ionicons';
+import { arrowBackOutline } from 'ionicons/icons';
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+// Registra el icono arrow-back-outline
+addIcons({
+  'arrow-back-outline': arrowBackOutline
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
